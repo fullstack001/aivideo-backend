0b7fd47181be583add1227cbb96064fac331eb4f
fullstack001 <mr.dimas332@gmail.com>
2024-10-07 16:44:07 +0300

    test

--- routes/api/videoCreate.js
+++ routes/api/videoCreate.js
@@ -83,14 +83,17 @@ router.post("/create-video", async (req, res) => {
     },
   };
 
-  try {
-    const response = await axios(options);
-    console.log(response.data);
-    res.json({ resultData: response.data });
-  } catch (error) {
-    console.error("Error creating video:", error);
-    res.status(500).json({ error: "Error creating video" });
-  }
+  console.log(options);
+  res.json({ success: "success" });
+
+  // try {
+  //   const response = await axios(options);
+  //   console.log(response.data);
+  //   res.json({ resultData: response.data });
+  // } catch (error) {
+  //   console.error("Error creating video:", error);
+  //   res.status(500).json({ error: "Error creating video" });
+  // }
 });
 
 router.get("/video-created", async (req, res) => {
